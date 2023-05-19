ba949a67dd3d1e506595bbe2d00177d44ac3bfe5
stefano <baiardi10@gmail.com>
2023-05-19 17:47:06 +0200

    add users list

---
+++ lib/ui/pages/detail_extra_user.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class DetailExtraUserPage extends StatefulWidget {
+  const DetailExtraUserPage({super.key});
+
+  @override
+  State<DetailExtraUserPage> createState() => _DetailExtraUserPageState();
+}
+
+class _DetailExtraUserPageState extends State<DetailExtraUserPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(20),
+        child: Column(children: [
+          
+        ]),
+      ),
+    );
+  }
+}
