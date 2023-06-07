42ba00426f2a67e8461c732e0f731a2f90d11097
stefano <baiardi10@gmail.com>
2023-06-07 10:43:06 +0200

    add bottom

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import GoogleMaps
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +8,7 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyC2z-1szIJqgE6pdcWblK4H2WpxRAU6gF0")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
--- lib/ui/components/bottom_sheet_danger.dart
+++ lib/ui/components/bottom_sheet_danger.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:flutter/material.dart';
+import 'package:ti_ospito_io_app_flutter/utils/utils_method.dart';
 
 class BottomSheetDangerWidget extends StatefulWidget {
   const BottomSheetDangerWidget({super.key});
@@ -10,9 +12,13 @@ class BottomSheetDangerWidget extends StatefulWidget {
 }
 
 class _BottomSheetDangerWidgetState extends State<BottomSheetDangerWidget> {
+
+  Position? position = null;
+
   @override
   Widget build(BuildContext context) {
     return Container(
+      padding: EdgeInsets.all(20),
       height: MediaQuery.of(context).size.height * 0.85,
       decoration: new BoxDecoration(
         color: Colors.white,
@@ -23,7 +29,43 @@ class _BottomSheetDangerWidgetState extends State<BottomSheetDangerWidget> {
       ),
       child: Center(
         child: Column(
-          children: [Text('Modal bottomsheet')],
+          children: [
+            Text('Hey! Hai visto una situazione pericolante e vuoi segnalarla?', style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),),
+            SizedBox(height: 10,),
+            Row(
+              children: [
+                Container(
+                  width: MediaQuery.of(context).size.width / 1.5,
+                  child: TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                        borderSide: new BorderSide(color: Colors.red)),
+                    hintText: 'Indicaci dove...',
+                    labelText: 'Indicaci dove...',
+                    prefixIcon: const Icon(
+                      CupertinoIcons.location_solid,
+                      color: Colors.red,
+                    ),
+                    prefixText: ' ',
+                  ),
+                              ),
+                ),
+                SizedBox(width: 20,),
+                GestureDetector(
+                  onTap: () async {
+                   
+                    determinePosition().then((value) => position = value);
+                    print(position);
+                  },
+                  child: Container(
+                    padding: EdgeInsets.all(10),
+                    decoration: BoxDecoration(color: Color.fromARGB(255, 191, 65, 24), borderRadius: BorderRadius.circular(10)),
+                    child: Icon(CupertinoIcons.location_circle, color: Colors.white,),
+                  ),
+                )
+              ],
+            )
+          ],
         ),
       ),
     );
